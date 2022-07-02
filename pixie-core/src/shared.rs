18a78e5abdd72df8cee76a2492f862b6ed868954
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2022-07-02 16:28:38 +0200

    Add files structures

--- pixie-core/src/shared.rs
+++ pixie-core/src/shared.rs
@@ -1,3 +1,6 @@
+use std::path::PathBuf;
+
+use blake3::OUT_LEN;
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
@@ -11,4 +14,18 @@ pub struct RegistrationInfo {
     pub groups: Vec<Group>,
     pub candidate_group: String,
     pub candidate_position: Vec<u8>,
+}
+
+/// The hash of a chunk of a file.
+///
+/// This is stored as an array of bytes because [`blake3::Hash`] is not serializable.
+pub type ChunkHash = [u8; OUT_LEN];
+/// The offset of the chunk of a file.
+pub type Offset = usize;
+
+/// A file is stored as a list of chunks and offsets.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct File {
+    pub name: PathBuf,
+    pub chunks: Vec<(ChunkHash, Offset)>,
 }
