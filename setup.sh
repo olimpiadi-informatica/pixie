f9a84bdf8fcc45c23b95b6dc7b4b493c82d82ec0
Filippo Casarin <casarin.filippo17@gmail.com>
2022-11-16 16:51:42 +0100

    Update setup

--- setup.sh
+++ setup.sh
@@ -17,24 +17,24 @@ cp uefi-reboot-skip/target/x86_64-unknown-uefi/release/uefi_app.efi storage/http
 mkdir -p storage/tftpboot
 [ -f storage/tftpboot/ipxe.efi ] || wget https://boot.ipxe.org/ipxe.efi -O storage/tftpboot/ipxe.efi
 
-mkdir -p storage/images storage/chunks
+mkdir -p storage/images storage/chunks storage/tmp
 
 [ -f storage/config.yaml ] || cp pixie-core/example.config.yaml storage/config.yaml
 
-BUILDDIR=build
-mkdir -p $BUILDDIR
-
-BR2_VER=2022.08
-
-[ -f ${BUILDDIR}/buildroot-${BR2_VER}.tar.xz ] || wget https://buildroot.org/downloads/buildroot-${BR2_VER}.tar.xz -O ${BUILDDIR}/buildroot-${BR2_VER}.tar.xz
-
-[ -d ${BUILDDIR}/buildroot-${BR2_VER} ]  || tar xf ${BUILDDIR}/buildroot-${BR2_VER}.tar.xz -C ${BUILDDIR}
-
 if ! [ -f storage/httpstatic/bzImage ]
 then
+  BUILDDIR=build
+  mkdir -p $BUILDDIR
+
+  BR2_VER=2022.08
+
+  [ -f ${BUILDDIR}/buildroot-${BR2_VER}.tar.xz ] || wget https://buildroot.org/downloads/buildroot-${BR2_VER}.tar.xz -O ${BUILDDIR}/buildroot-${BR2_VER}.tar.xz
+
+  [ -d ${BUILDDIR}/buildroot-${BR2_VER} ]  || tar xf ${BUILDDIR}/buildroot-${BR2_VER}.tar.xz -C ${BUILDDIR}
+
   unset LD_LIBRARY_PATH
   tar c --exclude-vcs --exclude-vcs-ignores . | gzip > buildroot/pixie.tar.gz
-  make O=$PWD/${BUILDDIR}/buildroot-build BR2_EXTERNAL=$PWD/buildroot/ -C ${BUILDDIR}/buildroot-${BR2_VER} pixie2_defconfig 
-  make O=$PWD/${BUILDDIR}/buildroot-build -C ${BUILDDIR}/buildroot-${BR2_VER} -j$(nproc)
+  make O="$PWD/${BUILDDIR}/buildroot-build" BR2_EXTERNAL="$PWD/buildroot/" -C ${BUILDDIR}/buildroot-${BR2_VER} pixie2_defconfig
+  make O="$PWD/${BUILDDIR}/buildroot-build" -C ${BUILDDIR}/buildroot-${BR2_VER} -j$(nproc)
   cp ${BUILDDIR}/buildroot-build/images/bzImage storage/httpstatic/bzImage
 fi
