2efc58a5481112eaf085f34d13aa31d0486e136f
Filippo Casarin <casarin.filippo17@gmail.com>
2022-12-02 03:47:19 +0100

    pixie-client parses ext4 partition directly

--- pixie-client/src/push.rs
+++ pixie-client/src/push.rs
@@ -1,8 +1,7 @@
 use std::{
     fs::File,
-    io::{self, BufRead, BufReader, Read, Seek, SeekFrom, Write},
+    io::{self, Read, Seek, SeekFrom, Write},
     path::{Path, PathBuf},
-    process::{Command, Stdio},
 };
 
 use anyhow::{ensure, Context, Result};
@@ -136,84 +135,121 @@ impl FileSaver for RemoteFileSaver {
 }
 
 fn get_ext4_chunks(path: &str) -> Result<Option<Vec<ChunkInfo>>> {
-    let child = Command::new("dumpe2fs")
-        .arg(path)
-        .stdin(Stdio::null())
-        .stdout(Stdio::piped())
-        .stderr(Stdio::null())
-        .spawn()?;
-    let stdout = child.stdout.unwrap();
-    let mut lines = BufReader::new(stdout).lines();
+    fn le16(buf: &[u8], lo: usize) -> u16 {
+        (0..2).map(|i| (buf[lo + i] as u16) << (8 * i)).sum()
+    }
 
-    let block_size: usize = loop {
-        let line = match lines.next() {
-            Some(Ok(x)) => x,
-            Some(Err(e)) => return Err(e.into()),
-            None => return Ok(None),
-        };
+    fn le32(buf: &[u8], lo: usize) -> u32 {
+        (0..4).map(|i| (buf[lo + i] as u32) << (8 * i)).sum()
+    }
 
-        if let Some(value) = line.strip_prefix("Block size:") {
-            break value.trim().parse().unwrap();
+    fn le64_32_32(buf: &[u8], lo: usize, hi: usize) -> u64 {
+        (0..4)
+            .map(|i| ((buf[lo + i] as u64) << (8 * i)) + ((buf[hi + i] as u64) << (8 * i + 32)))
+            .sum()
+    }
+
+    fn has_superblock(group: usize) -> bool {
+        if group <= 1 {
+            return true;
         }
-    };
+
+        for d in [3, 5, 7] {
+            let mut p = 1;
+            while p < group {
+                p *= d;
+            }
+            if p == group {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    let mut reader = File::open(path).unwrap();
+    let mut superblock = [0; 1024];
+    reader.seek(SeekFrom::Start(1024))?;
+    reader.read_exact(&mut superblock)?;
+
+    let magic = le16(&superblock, 0x38);
+    if magic != 0xEF53 {
+        return Ok(None);
+    }
+
+    let feature_incompat = le32(&superblock, 0x60);
+    if feature_incompat & 0x80 == 0 {
+        // INCOMPAT_64BIT flag
+        return Ok(None);
+    }
+
+    let feature_ro_compat = le32(&superblock, 0x64);
+    if feature_ro_compat & 0x1 == 0 {
+        // RO_COMPAT_SPARSE_SUPER flag
+        return Ok(None);
+    }
+
+    let blocks_count = le64_32_32(&superblock, 0x4, 0x150);
+    let log_block_size = le32(&superblock, 0x18);
+    assert!(blocks_count.checked_shl(10 + log_block_size).is_some());
+    let block_size = 1u64 << (10 + log_block_size);
+
+    let blocks_per_group = le32(&superblock, 0x20) as u64;
+    let groups = (blocks_count + blocks_per_group - 1) / blocks_per_group;
+
+    let first_data_block = le32(&superblock, 0x14) as u64;
+    let desc_size = le16(&superblock, 0xfe) as u64;
+    let reserved_gdt_blocks = le16(&superblock, 0xce);
+
+    let blocks_for_special_group = 1
+        + ((desc_size * groups + block_size - 1) / block_size) as usize
+        + reserved_gdt_blocks as usize;
+
+    let mut group_descriptors = vec![0; (desc_size * groups) as usize];
+    let mut bitmap = vec![0; block_size as usize];
+    reader
+        .seek(SeekFrom::Start(block_size * (first_data_block + 1)))
+        .unwrap();
+    reader.read_exact(&mut group_descriptors)?;
 
     let mut ans = Vec::new();
 
-    while !lines.next().unwrap()?.is_empty() {}
-
-    loop {
-        let (mut begin, end): (usize, usize) = loop {
-            let line = match lines.next() {
-                Some(Ok(x)) => x,
-                Some(Err(e)) => return Err(e.into()),
-                None => return Ok(Some(ans)),
-            };
-
-            if let Some(s) = line.strip_prefix("Group") {
-                let a = s.find('(').unwrap();
-                let b = s.find('-').unwrap();
-                let c = s.find(')').unwrap();
-                break (
-                    s[a + 8..b].parse().unwrap(),
-                    s[b + 1..c].parse::<usize>().unwrap() + 1,
-                );
-            }
-        };
-
-        loop {
-            let line = lines.next().unwrap()?;
-
-            if let Some(s) = line.strip_prefix("  Free blocks: ") {
-                if !s.is_empty() {
-                    for x in s.split(", ") {
-                        let (a, b) = if let Some(m) = x.find('-') {
-                            let a: usize = x[..m].parse().unwrap();
-                            let b: usize = x[m + 1..].parse().unwrap();
-                            (a, b + 1)
-                        } else {
-                            let a = x.parse().unwrap();
-                            (a, a + 1)
-                        };
-
-                        if begin < a {
-                            ans.push(ChunkInfo {
-                                start: block_size * begin,
-                                size: block_size * (a - begin),
-                            });
-                        }
-                        begin = b;
+    for (group, group_descriptor) in group_descriptors.chunks(desc_size as usize).enumerate() {
+        let flags = le16(group_descriptor, 0x12);
+        if flags & 0x2 != 0 {
+            // EXT4_BG_BLOCK_UNINIT
+            if has_superblock(group) {
+                for block in 0..blocks_for_special_group {
+                    if group * blocks_per_group as usize + block < blocks_count as usize {
+                        ans.push(ChunkInfo {
+                            start: block_size as usize
+                                * (group * blocks_per_group as usize + block),
+                            size: block_size as usize,
+                        });
                     }
                 }
-                if begin < end {
+            }
+        } else {
+            let block_bitmap = le64_32_32(group_descriptor, 0x0, 0x20);
+
+            reader
+                .seek(SeekFrom::Start(block_size * block_bitmap))
+                .unwrap();
+            reader.read_exact(&mut bitmap)?;
+
+            for block in 0..8 * block_size as usize {
+                let is_used = bitmap[block / 8] >> (block % 8) & 1 != 0;
+                if is_used && group * blocks_per_group as usize + block < blocks_count as usize {
                     ans.push(ChunkInfo {
-                        start: block_size * begin,
-                        size: block_size * (end - begin),
+                        start: block_size as usize * (group * blocks_per_group as usize + block),
+                        size: block_size as usize,
                     });
                 }
-                break;
             }
         }
     }
+
+    Ok(Some(ans))
 }
 
 fn get_disk_chunks(path: &str) -> Result<Option<Vec<ChunkInfo>>> {
@@ -294,6 +330,7 @@ fn get_file_chunks(path: &str) -> Result<Vec<ChunkInfo>> {
         let end = start + size;
 
         if let Some(last) = out.last() {
+            assert!(last.start + last.size <= start);
             if last.start + last.size == start {
                 start = last.start;
                 out.pop();
@@ -336,7 +373,7 @@ pub fn main() -> Result<()> {
         let total_size: usize = chunks.iter().map(|x| x.size).sum();
         println!("Total size: {}", total_size);
 
-        let mut file = std::fs::File::open(&s)?;
+        let mut file = File::open(&s)?;
 
         let total = chunks.len();
 
