50214c40c60094bb59cf053b33a90eba95798e48
Luca Versari <veluca93@gmail.com>
2022-12-26 22:02:50 +0100

    Fix setup.sh script for web

--- setup.sh
+++ setup.sh
@@ -18,7 +18,8 @@ popd
 
 mkdir -p storage/tftpboot
 cp pixie-uefi/target/x86_64-unknown-uefi/release/uefi_app.efi storage/tftpboot/
-cp -r pixie-web/dist storage/admin/
+mkdir -p storage/admin
+cp -r pixie-web/dist/* storage/admin/
 
 mkdir -p storage/images storage/chunks
 
