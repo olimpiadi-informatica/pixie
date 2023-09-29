1fca0102a02b1d47c353f376511f4d25baa0fb2a
Filippo Casarin <casarin.filippo17@gmail.com>
2023-09-29 02:11:36 +0200

    Parse ntfs partitions

--- pixie-uefi/src/push/parse_disk.rs
+++ pixie-uefi/src/push/parse_disk.rs
@@ -1,7 +1,10 @@
 use alloc::vec::Vec;
 
 use crate::{
-    os::{disk::Disk, error::Result},
+    os::{
+        disk::Disk,
+        error::{Error, Result},
+    },
     push::ChunkInfo,
 };
 
@@ -11,6 +14,10 @@ fn le16(buf: &[u8], lo: usize) -> u16 {
 
 fn le32(buf: &[u8], lo: usize) -> u32 {
     (0..4).map(|i| (buf[lo + i] as u32) << (8 * i)).sum()
+}
+
+fn le64(buf: &[u8], lo: usize) -> u64 {
+    (0..8).map(|i| (buf[lo + i] as u64) << (8 * i)).sum()
 }
 
 fn le64_32_32(buf: &[u8], lo: usize, hi: usize) -> u64 {
@@ -126,7 +133,96 @@ async fn get_ext4_chunks(disk: &Disk, start: u64, end: u64) -> Result<Option<Vec
 }
 
 async fn get_ntfs_chunks(disk: &Disk, start: u64, end: u64) -> Result<Option<Vec<ChunkInfo>>> {
-    Ok(None)
+    if end - start < 512 {
+        return Ok(None);
+    }
+
+    let mut boot_sector = [0u8; 512];
+    disk.read(start, &mut boot_sector).await?;
+
+    if &boot_sector[3..11] != b"NTFS    " {
+        return Ok(None);
+    }
+
+    let bytes_per_sector = le16(&boot_sector, 0x0b) as usize;
+
+    let sectors_per_cluster = match boot_sector[0x0d] {
+        x @ 0..=127 => x as usize,
+        x @ 225..=255 => 1 << -(x as i8),
+        x @ 128..=224 => panic!("too many sectors per cluster: {}", x),
+    };
+    let bytes_per_cluster = bytes_per_sector * sectors_per_cluster;
+    let num_clusters = (end as usize - start as usize + bytes_per_cluster - 1) / bytes_per_cluster;
+
+    let bytes_per_file_record = match boot_sector[0x40] {
+        x @ 0..=127 => x as usize * bytes_per_cluster,
+        x @ 225..=255 => 1 << -(x as i8),
+        x @ 128..=224 => panic!("too many bytes per file record: {}", x),
+    };
+
+    let mft_cluster_number = le64(&boot_sector, 0x30) as usize;
+    let mft_address = bytes_per_cluster * mft_cluster_number;
+
+    let bitmap_entry_address = mft_address + 6 * bytes_per_file_record;
+    let mut bitmap_entry = [0u8; 1024];
+    disk.read(start + bitmap_entry_address as u64, &mut bitmap_entry)
+        .await
+        .map_err(|e| Error::Generic(format!("failed to read bitmap entry: {}", e)))?;
+
+    let mut attribute_offset = le16(&bitmap_entry, 0x14) as usize;
+    while le32(&bitmap_entry, attribute_offset) != 0x80 {
+        attribute_offset += le32(&bitmap_entry, attribute_offset + 4) as usize;
+    }
+
+    let non_resident_flag = bitmap_entry[attribute_offset + 8];
+    assert_eq!(non_resident_flag, 1);
+
+    let mut start_vcn = le64(&bitmap_entry, attribute_offset + 0x10) as usize;
+    let last_vcn = le64(&bitmap_entry, attribute_offset + 0x18) as usize;
+    let mut data_run_offset =
+        attribute_offset + le16(&bitmap_entry, attribute_offset + 0x20) as usize;
+
+    let mut cnt = 0;
+    let mut chunks = Vec::new();
+
+    while start_vcn <= last_vcn {
+        let ctrl_byte = bitmap_entry[data_run_offset];
+
+        let length_len = (ctrl_byte & 0x0f) as usize;
+        let length =
+            (le64(&bitmap_entry, data_run_offset + 1) & ((1 << (8 * length_len)) - 1)) as usize;
+
+        let offset_len = (ctrl_byte >> 4) as usize;
+        let offset = (le64(&bitmap_entry, data_run_offset + 1 + length_len)
+            & ((1 << (8 * offset_len)) - 1)) as usize;
+
+        let mut buf = vec![0u8; bytes_per_cluster];
+        for i in 0..length {
+            let x = start + (offset + i) as u64 * bytes_per_cluster as u64;
+            disk.read(x, &mut buf).await.map_err(|e| {
+                Error::Generic(format!("failed to read bitmap content at {}: {}", x, e))
+            })?;
+
+            for &byte in &buf {
+                for bit in 0..8 {
+                    if cnt < num_clusters as u64 {
+                        if byte >> bit & 1 != 0 {
+                            chunks.push(ChunkInfo {
+                                start: cnt as usize * bytes_per_cluster,
+                                size: bytes_per_cluster,
+                            });
+                        }
+                        cnt += 1;
+                    }
+                }
+            }
+        }
+
+        start_vcn += length;
+        data_run_offset += 1 + length_len + offset_len;
+    }
+
+    Ok(Some(chunks))
 }
 
 /// Returns chunks *relative to the start of the partition*.
@@ -160,7 +256,7 @@ async fn parse_gpt(disk: &mut Disk) -> Result<Option<Vec<ChunkInfo>>> {
             });
         }
 
-        let part_chunks = parse_partition(&disk, begin as u64, end as u64).await?;
+        let part_chunks = parse_partition(disk, begin as u64, end as u64).await?;
         for ChunkInfo { start, size } in part_chunks {
             chunks.push(ChunkInfo {
                 start: start + begin,
