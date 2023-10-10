33282ca28a3bb312ee84b9f90489a95dae9a2bc1
Luca Versari <veluca93@gmail.com>
2023-10-10 14:09:56 +0200

    More robust reboot-to-os command

--- pixie-uefi/src/reboot_to_os.rs
+++ pixie-uefi/src/reboot_to_os.rs
@@ -1,16 +1,28 @@
-use crate::os::UefiOS;
+use crate::os::{MessageKind, UefiOS};
 
 pub async fn reboot_to_os(os: UefiOS) -> ! {
     let bo = os.boot_options();
     let order = bo.order();
-    let current_index = order
+    let num_skip = order
         .iter()
         .cloned()
-        .enumerate()
-        .find(|x| x.1 == bo.current())
-        .unwrap_or((0, 0))
-        .0;
-    // Reboot to next boot option.
-    bo.set_next(order[(current_index + 1).min(order.len() - 1)]);
+        .position(|x| x == bo.current())
+        .map(|x| x + 1)
+        .unwrap_or(0);
+    let next = order.iter().cloned().skip(num_skip).find(|x| *x < 0x2000);
+    if let Some(next) = next {
+        // Reboot to next boot option.
+        bo.set_next(next);
+    } else {
+        os.append_message(
+            format!(
+                "Did not find a valid boot order entry! current: {}",
+                bo.current()
+            ),
+            MessageKind::Warning,
+        );
+        os.append_message(format!("{:?}", order), MessageKind::Warning);
+        os.sleep_us(100_000_000).await;
+    }
     os.reset();
 }
