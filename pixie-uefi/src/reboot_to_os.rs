2f6bad402286ec81d9b991580ebdd66598f382ca
Luca Versari <veluca93@gmail.com>
2023-10-10 13:16:02 +0200

    Try to fix reboot_to_os if PXE is not entry 0.

--- pixie-uefi/src/reboot_to_os.rs
+++ pixie-uefi/src/reboot_to_os.rs
@@ -3,7 +3,14 @@ use crate::os::UefiOS;
 pub async fn reboot_to_os(os: UefiOS) -> ! {
     let bo = os.boot_options();
     let order = bo.order();
-    // Reboot to second boot option.
-    bo.set_next(order[1]);
+    let current_index = order
+        .iter()
+        .cloned()
+        .enumerate()
+        .find(|x| x.1 == bo.current())
+        .unwrap_or((0, 0))
+        .0;
+    // Reboot to next boot option.
+    bo.set_next(order[(current_index + 1).min(order.len() - 1)]);
     os.reset();
 }
