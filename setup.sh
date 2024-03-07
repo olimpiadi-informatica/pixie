5b4ec575e919d1765266ac73c33effb0d5f46c89
Filippo Casarin <casarin.filippo17@gmail.com>
2024-03-07 19:19:12 +0100

    using trunk asset support

--- setup.sh
+++ setup.sh
@@ -25,8 +25,6 @@ mkdir -p "${STORAGE_DIR}/tftpboot"
 cp pixie-uefi/target/x86_64-unknown-uefi/release/pixie-uefi.efi "${STORAGE_DIR}/tftpboot/"
 mkdir -p "${STORAGE_DIR}/admin"
 cp -r pixie-web/dist/* "${STORAGE_DIR}/admin/"
-cp pixie-web/style.css "${STORAGE_DIR}/admin/"
-cp pixie-web/favicon.ico "${STORAGE_DIR}/admin/"
 
 mkdir -p "${STORAGE_DIR}/images" "${STORAGE_DIR}/chunks"
 
