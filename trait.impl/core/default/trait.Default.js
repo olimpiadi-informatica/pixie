813a615014c848f500890caec932fed587bc90a9
Virv12 <55024474+Virv12@users.noreply.github.com>
2025-05-26 17:11:07 +0000

    Deploying to gh-pages from @ olimpiadi-informatica/pixie@6b96c5fa4e7f688f457f953f5d673bd06cf14f5c 🚀

--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["pixie_shared",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"pixie_shared/struct.Station.html\" title=\"struct pixie_shared::Station\">Station</a>"],["impl&lt;T, U&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"pixie_shared/bijection/struct.Bijection.html\" title=\"struct pixie_shared::bijection::Bijection\">Bijection</a>&lt;T, U&gt;"]]]]);
+    var implementors = Object.fromEntries([["pixie_shared",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"pixie_shared/struct.RegistrationInfo.html\" title=\"struct pixie_shared::RegistrationInfo\">RegistrationInfo</a>"],["impl&lt;T, U&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"pixie_shared/bijection/struct.Bijection.html\" title=\"struct pixie_shared::bijection::Bijection\">Bijection</a>&lt;T, U&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[609]}
+//{"start":57,"fragment_lengths":[636]}
--- trait.impl/core/marker/trait.Copy.js
+++ trait.impl/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["pixie_shared",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"pixie_shared/enum.Action.html\" title=\"enum pixie_shared::Action\">Action</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"pixie_shared/struct.Chunk.html\" title=\"struct pixie_shared::Chunk\">Chunk</a>"]]]]);
+    var implementors = Object.fromEntries([["pixie_shared",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"enum\" href=\"pixie_shared/enum.Action.html\" title=\"enum pixie_shared::Action\">Action</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"pixie_shared/struct.Chunk.html\" title=\"struct pixie_shared::Chunk\">Chunk</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
