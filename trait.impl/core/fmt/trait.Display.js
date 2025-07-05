c8693c481bbfd3fe730e0c8c7fc9827ef7ee6223
veluca93 <veluca93@gmail.com>
2025-07-05 15:06:05 +0000

    Deploying to gh-pages from @ olimpiadi-informatica/pixie@4f2f83056a68ae3115646084462e4e5d12acfb70 🚀

--- trait.impl/core/fmt/trait.Display.js
+++ trait.impl/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["pixie_shared",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"pixie_shared/enum.Action.html\" title=\"enum pixie_shared::Action\">Action</a>"]]],["pixie_web",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"pixie_web/struct.Bytes.html\" title=\"struct pixie_web::Bytes\">Bytes</a>"]]]]);
+    var implementors = Object.fromEntries([["pixie_shared",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"pixie_shared/enum.Action.html\" title=\"enum pixie_shared::Action\">Action</a>"]]],["pixie_web",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"pixie_web/struct.Bytes.html\" title=\"struct pixie_web::Bytes\">Bytes</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
