83b48f55831b3e17cbb5f18f4b98d248c5ef10ee
Luca Versari <veluca93@gmail.com>
2026-09-24 14:40:53 +0200

    net/tcp: remove 1.4MB/s bottleneck by disabling Cubic/Nagle and enabling burst egress polling

--- pixie-uefi/src/os/net/mod.rs
+++ pixie-uefi/src/os/net/mod.rs
@@ -295,7 +295,16 @@ fn poll() -> Option<u64> {
         dhcp_socket_handle,
     } = data.as_mut().unwrap();
 
-    let status_out = interface.poll_egress(now, device, socket_set);
+    let mut num_egress = 0;
+    while device.nic.can_transmit()
+        && interface.poll_egress(now, device, socket_set) != PollResult::None
+    {
+        num_egress += 1;
+        if num_egress >= 64 {
+            break;
+        }
+    }
+
     let mut num_ingress = 0;
     while interface.poll_ingress_single(now, device, socket_set) != PollIngressSingleResult::None {
         num_ingress += 1;
@@ -331,7 +340,12 @@ fn poll() -> Option<u64> {
         }
     }
 
-    if num_ingress > 0 || status_out != PollResult::None || device.nic.has_packets() {
+    let has_pending_tx = socket_set.iter().any(|(_, socket)| match socket {
+        smoltcp::socket::Socket::Tcp(s) => s.send_queue() > 0,
+        _ => false,
+    });
+
+    if num_ingress > 0 || num_egress > 0 || has_pending_tx || device.nic.has_packets() {
         return Some(0);
     }
 
